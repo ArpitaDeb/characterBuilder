98d906611e84ab23f0f1601a2d355695cbe02c1a
Jesse Witow <jwitow@gmail.com>
2018-07-18 18:01:23 -0600

    redid basic grid layout

---
+++ main.js
@@ -0,0 +1,29 @@
+.pageGrid-container {
+display: grid;
+}
+
+.pageGrid-item {
+
+}
+
+.optionsGrid-container {
+display:grid;
+}
+
+.optionsGrid-item {
+
+}
+
+.charClass {
+
+}
+
+#portraitContainer {
+    grid-column: 1;
+    grid-row: 1;
+}
+
+#specsContainer {
+    grid-column: 2 / 2;
+    grid-row: 1;
+}
