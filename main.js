360e24e5d065f9554967518ce75dcc9a7c2b079a
Jesse Witow <jwitow@gmail.com>
2018-07-27 15:12:37 -0600

    messing around with single/multi forms, visible/hidden sections

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    console.log('Test?');
+
+
+    const btnCharClassSubmit = document.getElementById('charClassSubmit')
+
+    btnCharClassSubmit.onclick = function () {
+        console.log('You\'ve clicked the Class Submit button');
+    }
+}
