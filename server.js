07eaeaa43c94eba6f8a0cce704c52f139f75aaaf
Jesse Witow <jwitow@gmail.com>
2018-08-22 18:03:23 -0600

    fixed server.js to serve up entire "public" folder

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'));
+// });
 
 app.post('/', (req, res) => {
     res.send('This is a POST');
